8cf997f09110ca4c5497dd7a6ac5d9669f89c009
snzsply <snzsply>
2018-06-12 15:21:58 +0000

    History + snapshots

--- bot/messageUsers.js
+++ bot/messageUsers.js
@@ -3,7 +3,7 @@ const { DISPATCH_LEVELS, dispatchNotifications } = require("./notify");
 async function main() {
   await dispatchNotifications([
     {
-      body: `Sorry for the false alarm. It was caused by the new yeezysupply.com homepage design. I have updated the code to prevent this.`,
+      body: `Sorry for another false alarm. I'm pausing snoozesupply until I can make it more reliable with the new yeezysupply design. Thanks for understanding.`,
       level: DISPATCH_LEVELS.ALL_USERS,
       method: "message"
     }
